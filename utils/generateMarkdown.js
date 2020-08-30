50369734bd240586e444904e92438ce3883aef76
berhane beyene <monoxica2004@yahoo.com>
2020-08-29 23:07:32 -0500

    README markdown commit

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,30 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+   ## Description
+   ${data.description}
+   ## Table of Contents
+   * [Installation Instructions](#installation-instructions)
+   
+   * [Usage Instructions](#usage-instructions)
+   
+   * [Contribution](#Contribution)
+   
+   * [Developers Contact Information](#Developers-Contact-Information)
+     ${licenseLink(data.license)}
+   ## Installation Instructions
+   ${data.installation}
+   ## Usage Instructions
+   ${data.usage}
+   ## Contributiion
+   ${data.contribution}
+   
+   ## Developers Contact Information
+    * LinkdIn Profile: ${data.linkdin}
+    * URL : ${data.url}
+    * Email: ${data.email}
+   ${displayLicense(data.license)}
+  `;
 }
 
 module.exports = generateMarkdown;
